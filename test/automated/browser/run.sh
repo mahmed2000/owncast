b3af8d2b25c4ab774449a49d4ead120fc79c8aa9
Muaz Ahmad <mahmad2000@protonmail.com>
2024-11-18 16:03:04 +0500

    Background the tunnel, tests need to wait for results before exiting.

--- test/automated/browser/run.sh
+++ test/automated/browser/run.sh
@@ -7,10 +7,18 @@ source ../tools.sh
 
 BUILD_ID=$((RANDOM % 7200 + 600))
 
+finish() {
+	kill_with_kids "$LT_TUNNEL_PID"
+	kill_with_kids "$STREAM_PID"
+}
+
 curl -o ./LT.zip https://downloads.lambdatest.com/tunnel/v3/linux/64bit/LT_Linux.zip
 unzip -o ./LT.zip
 # Use BUILD_ID to set the tunnel name, instead of having to parse out a random uuid.
-./LT --user "$LT_USER" --key "$LT_ACCESS_KEY" --tunnelName "$BUILD_ID"
+./LT --user "$LT_USER" --key "$LT_ACCESS_KEY" --tunnelName "$BUILD_ID" &
+LT_TUNNEL_PID=$!
+
+tap finish EXIT TERM INT
 
 # Bundle the updated web code into the server codebase.
 if [ -z "$SKIP_BUILD" ]; then
@@ -41,10 +49,12 @@ install_ffmpeg
 
 start_owncast
 
-npx lambdatest-cypress run --build-name "desktop-offline-$BUILD_ID" --tags "desktop,offline" --envs "tags=desktop" --build-identifier "$BUILD_ID" --specs "./cypress/e2e/offline/*.cy.js" --tname "$BUILD_ID"
-npx lambdatest-cypress run --build-name "mobile-offline-$BUILD_ID" --tags "mobile,offline" --envs "tags=mobile" --build-identifier "$BUILD_ID" --specs "./cypress/e2e/offline/*.cy.js" --tname "$BUILD_ID" --res "375x667"
+npx lambdatest-cypress run --build-name "desktop-offline-$BUILD_ID" --tags "desktop,offline" --envs "tags=desktop" --build-identifier "$BUILD_ID" --specs "./cypress/e2e/offline/*.cy.js" --tname "$BUILD_ID" --autostart "false" --sync "true"
+npx lambdatest-cypress run --build-name "mobile-offline-$BUILD_ID" --tags "mobile,offline" --envs "tags=mobile" --build-identifier "$BUILD_ID" --specs "./cypress/e2e/offline/*.cy.js" --tname "$BUILD_ID" --res "375x667" --autostart "false" --sync "true"
 
 start_stream
+sleep 20
 
-npx lambdatest-cypress run --build-name "desktop-online-$BUILD_ID" --tags "desktop,online" --envs "tags=desktop" --build-identifier "$BUILD_ID" --specs "./cypress/e2e/online/*.cy.js" --tname "$BUILD_ID"
-npx lambdatest-cypress run --build-name "mobile-online-$BUILD_ID" --tags "mobile,online" --envs "tags=mobile" --build-identifier "$BUILD_ID" --specs "./cypress/e2e/online/*.cy.js" --tname "$BUILD_ID" --res "375x667"
+npx lambdatest-cypress run --build-name "desktop-online-$BUILD_ID" --tags "desktop,online" --envs "tags=desktop" --build-identifier "$BUILD_ID" --specs "./cypress/e2e/online/*.cy.js" --tname "$BUILD_ID" --autostart "false" --sync "true"
+npx lambdatest-cypress run --build-name "mobile-online-$BUILD_ID" --tags "mobile,online" --envs "tags=mobile" --build-identifier "$BUILD_ID" --specs "./cypress/e2e/online/*.cy.js" --tname "$BUILD_ID" --res "375x667" --autostart "false" --sync "true"
+
